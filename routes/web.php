206d6e06c0ba6d532b8b4699203245a96daf5d46
Diego Termite <diegotermite@gmail.com>
2022-06-02 21:55:20 -0300

    agregada ruta a web.php

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 Route::get('/empleado', function () {
     return view('empleado.index');
 });
-route::get('empleado/create',);
+route::get('empleado/create',[EmpleadoController::class,'create']);
