203d40f9dda91620169886dc9db0f33ac5323a0c
Diego Termite <diegotermite@gmail.com>
2022-06-10 21:49:33 -0300

    intento de update fallido

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,22 +1,23 @@
 <!--Formulario que tendra datos en comun entre edit y create-->
 <label for="nombre">Nombre</label>
-    <input type="text" name="nombre" id="nombre">
+    <input type="text" name="nombre" value="{{$empleado->nombre}}" id="nombre">
     <br>
 
     <label for="apellidoPaterno">Apellido Paterno</label>
-    <input type="text" name="apellidoPaterno" id="apellidoPaterno">
+    <input type="text" name="apellidoPaterno" value="{{$empleado->apellidoPaterno}}" id="apellidoPaterno">
     <br>
 
     <label for="apellidoMaterno">Apellido Materno</label>
-    <input type="text" name="apellidoMaterno" id="apellidoMaterno">
+    <input type="text" name="apellidoMaterno" value="{{$empleado->apellidoMaterno}}" id="apellidoMaterno">
     <br>
 
     <label for="correo">E-mail</label>
-    <input type="email" name="correo" id="correo">
+    <input type="email" name="correo" value="{{$empleado->correo}}"id="correo">
     <br>
 
     <label for="foto">Foto</label>
-    <input type="file" name="foto" id="foto">
+    {{$empleado->foto}}
+    <input type="file" name="foto" value="" id="foto">
     <br>
 
     <input type="submit" value="Guardar Datos">
